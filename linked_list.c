32a85960e7836fb72029bf6e89f58ac263eeeae0
Shifananasrin <shifananasrin941@gmail.com>
2024-11-15 15:09:27 +0530

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -39,14 +39,131 @@ void insertAtFront(int data)
 		header=newnode;
 	}
 }
-	
+//insertatend
+void insertAtEnd(int data)
+{
+	struct Node *newnode;
+	struct Node *ptr;
+	newnode=createNode(data);
+	ptr=header->link;
+	while(ptr->link!=NULL)
+	{
+		ptr=ptr->link;
+	}
+		newnode=createNode(data);
+		newnode->data=data;
+		newnode->link=NULL;
+		ptr->link=newnode;
+		
+}
+//insertionatany
+void insertionAtAny(int data)
+{
+	struct Node *newnode;
+	struct Node *ptr;
+	ptr=header;
+	while((ptr!=NULL) && (ptr->data!=data))
+	{
+		ptr=ptr->link;
+	}
+	if(ptr==NULL)
+	{
+		printf("key is node found, insertion is not possible");
+	}
+	newnode=createNode(data);
+	newnode->link=ptr->link;
+	ptr->link=newnode;
+	newnode->data=data;
+}
+//traversal function for checking the insertion are correct
+void traversallist()
+{
+	struct Node *ptr;
+	ptr=header;
+	while(ptr!=NULL)
+	{
+		printf("%d",ptr->data);
+		ptr=ptr->link;
+	}
+	printf("\n");
+}
+
+//deletion at front
+void deletionAtFront()
+{
+	struct Node *ptr;
+	if(header==NULL)
+	{
+		printf("list is empty");
+	}
+	else
+	{
+		
+	        ptr=header;
+	        header=header->link;
+	        free(ptr);
+	}
+		
+}
+// deletion at end
+void deletionAtEnd()
+{
+	struct Node *ptr;
+	struct Node *ptr1;
+	if(header==NULL)
+	{
+		printf("list is empty");
+	}
+	while(ptr->link!=NULL)
+	{
+		ptr1=prt;
+		ptr=ptr->link;
+	}
+	ptr->link=NULL;
+	free(ptr);	
+}
 	
 int main()
 {
-	struct Node *Node;
-	insertAtfront(10);
-	insertAtfrot(20);
+	//struct Node *Node;
+	insertAtFront(10);
+	insertAtFront(20);
+	insertAtEnd(30);
+	insertionAtAny(10);
+	traversallist();
+	deletionAtFront();
+	traversallist();
+	deletionAtEnd()
+	traversallist();
 	return(0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
